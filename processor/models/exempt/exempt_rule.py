445138439ecf99762e0e0dc35a3558c7cd28f44b
PraveenMewada02 <pravinmewada1999@gmail.com>
2025-09-05 15:27:44 +0530

    exempt rule model

--- processor/models/exempt/exempt_rule.py
+++ processor/models/exempt/exempt_rule.py
@@ -2,7 +2,6 @@ from django.db import models
 
 class ExemptRule(models.Model):
     state = models.ForeignKey('processor.State', on_delete=models.CASCADE, db_index=True)
-    rule_id = models.IntegerField(unique=True,db_index=True)
     deduction_basis = models.CharField(max_length=255, blank=True, null=True)
     withholding_limit = models.CharField(max_length=255, blank=True, null=True)
     rule = models.CharField(max_length=2500, blank=True, null=True)
