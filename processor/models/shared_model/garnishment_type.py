3ed3de6ee4c2c8395b4890cbdb65543335dc4960
Praveen Mewada <pravinmewada1999@gmail.com>
2025-11-11 13:20:57 +0530

    gar_type table migrations

--- processor/models/shared_model/garnishment_type.py
+++ processor/models/shared_model/garnishment_type.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 class GarnishmentType(models.Model):
+    code= models.CharField(max_length=10, blank =True, null=True)
     type = models.CharField(max_length=100, unique=True)
     description = models.TextField(null=True, blank=True)
+    report_description= models.CharField(max_length=100,null=False)
+    pay_stub_description= models.CharField(max_length=100,null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -12,4 +15,6 @@ class GarnishmentType(models.Model):
     class Meta:
         db_table = "garnishment_type"
         verbose_name ="garnishment_type"
+
+
 
