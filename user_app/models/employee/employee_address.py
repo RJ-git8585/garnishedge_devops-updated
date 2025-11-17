39e46f73a2ceb24fcc74d2e35aa88122edbbd85b
Praveen Mewada <pravinmewada1999@gmail.com>
2025-11-17 14:21:32 +0530

    allow null in employee field

--- user_app/models/employee/employee_address.py
+++ user_app/models/employee/employee_address.py
@@ -3,12 +3,12 @@ from django.db import models
 class EmplopyeeAddress(models.Model):
     ee= models.OneToOneField('user_app.EmployeeDetail', on_delete=models.CASCADE, related_name="employee_addresses")
     address_1=models.TextField(max_length=255,null=False)
-    address_2=models.TextField(max_length=255,null=False)
+    address_2=models.TextField(max_length=255,blank=True, null=True)
     zip_code=models.IntegerField(null=False)
     geo_code=models.IntegerField(null=False)
     city=models.CharField(max_length=100,null=False)
     state=models.CharField(max_length=100,null=False)
-    county=models.CharField(max_length=100,null=False)
+    county=models.CharField(max_length=100,blank=True, null=True)
     country=models.CharField(max_length=100,null=False)
 
     class Meta:
